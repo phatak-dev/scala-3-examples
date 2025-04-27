79505e8988c514068217c43c48699ea079f92318
phatak.dev <phatak.dev@gmail.com>
2025-04-27 13:31:10 +0530

    improved summon example

--- src/main/scala/SummonExample.scala
+++ src/main/scala/SummonExample.scala
@@ -1,21 +1,28 @@
 package com.madhukaraphatak
 
-import ImplicitParameters.Comparator
 
 object SummonExample {
-  def main(args: Array[String]): Unit = {
-    trait Ord[T] {
-      def compare(x: T, y: T): Int
-    }
-    class IntOrdering extends Ord[Int] {
-      override def compare(x: Int, y: Int): Int = x.compare(y)
-    }
-    given intOrdering:Ord[Int] = new IntOrdering()
+  trait Ord[T] {
+    def compare(x: T, y: T): Int
+  }
 
-    def maxWithUsing(x:Int,y:Int):Int ={
+  class IntOrdering extends Ord[Int] {
+    override def compare(x: Int, y: Int): Int = x.compare(y)
+  }
+
+  def main(args: Array[String]): Unit = {
+    given intOrdering: Ord[Int] = new IntOrdering()
+
+    def maxWithImplicitParameter(x: Int, y: Int)(implicit intOrdering: Ord[Int]): Int = {
+      if (intOrdering.compare(x, y) < 0)   y else x
+    }
+    println(maxWithImplicitParameter(10,20))
+    //Scala 3.x
+    def maxWithSummon(x: Int, y: Int): Int = {
       //summon the implicit context using surrounding context
       val intOrdering = summon[Ord[Int]]
-      if intOrdering.compare(x,y) < 0 then y else x
+      if intOrdering.compare(x, y) < 0 then y else x
     }
+    println(maxWithSummon(10,20))
   }
 }
